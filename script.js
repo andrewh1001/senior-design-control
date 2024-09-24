9df276b488fa60808dd6d5aa20c11fd0115b917b
Andrew Huang <andrewh1001@gmail.com>
2024-09-24 16:17:08 -0400

    updated with arrows

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const upButton = document.getElementById('up');
 const leftButton = document.getElementById('left');
 const rightButton = document.getElementById('right');
 const downButton = document.getElementById('down');
+const toggleButton = document.getElementById('toggle-btn');
 
 var longpress = false;
 var presstimer = null;
@@ -84,40 +85,249 @@ rightButton.addEventListener("touchend", cancel);
 rightButton.addEventListener("touchleave", cancel);
 rightButton.addEventListener("touchcancel", cancel);
 
+const keyState = {
+    up: false,
+    down: false,
+    left: false,
+    right: false
+};
+
 document.addEventListener('keydown', function(e) {
-    switch (e.key) {
-        case "ArrowUp":
-            upButton.classList.add("longpress");
-            result.innerHTML = "Up key pressed";
-            break;
-        case "ArrowDown":
-            downButton.classList.add("longpress");
-            result.innerHTML = "Down key pressed";
-            break;
-        case "ArrowLeft":
-            leftButton.classList.add("longpress");
-            result.innerHTML = "Left key pressed";
-            break;
-        case "ArrowRight":
-            rightButton.classList.add("longpress");
-            result.innerHTML = "Right key pressed";
-            break;
+    // Enable/disable toggle button with "t"
+    if (e.key == "t") {
+        toggleButton.disabled = !toggleButton.disabled;
+        if (toggleButton.disabled) {
+            result.innerHTML = "Button Disabled";
+        } else {
+            result.innerHTML = "Button Enabled";
+        }
+        return;
+    }
+
+    // Handle movement keys if toggle button is enabled
+    if (!toggleButton.disabled) {
+        switch (e.key) {
+            case "ArrowUp":
+            case "w":
+                keyState.up = true;
+                break;
+            case "ArrowDown":
+            case "s":
+                keyState.down = true;
+                break;
+            case "ArrowLeft":
+            case "a":
+                keyState.left = true;
+                break;
+            case "ArrowRight":
+            case "d":
+                keyState.right = true;
+                break;
+        }
+        handleMovement();
     }
 });
 
 document.addEventListener('keyup', function(e) {
-    switch (e.key) {
-        case "ArrowUp":
-            upButton.classList.remove("longpress");
-            break;
-        case "ArrowDown":
-            downButton.classList.remove("longpress");
-            break;
-        case "ArrowLeft":
-            leftButton.classList.remove("longpress");
-            break;
-        case "ArrowRight":
-            rightButton.classList.remove("longpress");
-            break;
+    if (!toggleButton.disabled) {
+        switch (e.key) {
+            case "ArrowUp":
+            case "w":
+                keyState.up = false;
+                upButton.classList.remove("active");
+                break;
+            case "ArrowDown":
+            case "s":
+                keyState.down = false;
+                downButton.classList.remove("active");
+                break;
+            case "ArrowLeft":
+            case "a":
+                keyState.left = false;
+                leftButton.classList.remove("active");
+                break;
+            case "ArrowRight":
+            case "d":
+                keyState.right = false;
+                rightButton.classList.remove("active");
+                break;
+        }
+        handleMovement();
     }
 });
+
+function handleMovement() {
+    // Handle simultaneous pressing of keys
+    if (keyState.left && keyState.up) {
+        leftButton.classList.add("active");
+        upButton.classList.add("active");
+        result.innerHTML = "Up and Left keys pressed";
+        writePwmValue(65); // Custom PWM value for diagonal movement
+    } else if (keyState.right && keyState.up) {
+        rightButton.classList.add("active");
+        upButton.classList.add("active");
+        result.innerHTML = "Up and Right keys pressed";
+        writePwmValue(115); // Custom PWM value for diagonal movement
+    } else if (keyState.left && keyState.down) {
+        leftButton.classList.add("active");
+        downButton.classList.add("active");
+        result.innerHTML = "Down and Left keys pressed";
+        // Add logic here for simultaneous down + left
+    } else if (keyState.right && keyState.down) {
+        rightButton.classList.add("active");
+        downButton.classList.add("active");
+        result.innerHTML = "Down and Right keys pressed";
+        // Add logic here for simultaneous down + right
+    }
+    // Individual key presses
+    else if (keyState.left) {
+        leftButton.classList.add("active");
+        result.innerHTML = "Left key pressed";
+        writePwmValue(40);
+    } else if (keyState.right) {
+        rightButton.classList.add("active");
+        result.innerHTML = "Right key pressed";
+        writePwmValue(140);
+    } else if (keyState.up) {
+        upButton.classList.add("active");
+        result.innerHTML = "Up key pressed";
+        writePwmValue(90);
+    } else if (keyState.down) {
+        downButton.classList.add("active");
+        result.innerHTML = "Down key pressed";
+    }
+}
+
+
+
+// DOM Elements
+const connectButton = document.getElementById('connectBleButton');
+const disconnectButton = document.getElementById('disconnectBleButton');
+const setPwmButton = document.getElementById('setPwmButton');
+const pwmValueInput = document.getElementById('pwmValueInput');
+const latestValueSent = document.getElementById('valueSent');
+const bleStateContainer = document.getElementById('bleState');
+
+// Define BLE Device Specs
+var deviceName = 'ESP32';
+var bleService = '19b10000-e8f2-537e-4f6c-d104768a1214';
+var ledCharacteristic = '19b10002-e8f2-537e-4f6c-d104768a1214';
+
+// Global Variables to Handle Bluetooth
+var bleServer;
+var bleServiceFound;
+
+// Connect Button (search for BLE Devices)
+connectButton.addEventListener('click', () => {
+    if (isWebBluetoothEnabled()){
+        connectToDevice();
+    }
+});
+
+// Disconnect Button
+disconnectButton.addEventListener('click', disconnectDevice);
+
+// Set PWM Value Button
+setPwmButton.addEventListener('click', () => {
+    const pwmValue = parseInt(pwmValueInput.value);
+    if (pwmValue >= 0 && pwmValue <= 180) {
+        writePwmValue(pwmValue);
+    } else {
+        console.error("Invalid PWM value! Please enter a number between 0 and 255.");
+    }
+});
+
+forwardButton.addEventListener('click', () => writePwmValue(190));  // 2 for forward
+backwardButton.addEventListener('click', () => writePwmValue(191)); // 3 for backward
+stopButton.addEventListener('click', () => writePwmValue(192));     // 4 for stop
+
+// Check if BLE is available in your browser
+function isWebBluetoothEnabled() {
+    if (!navigator.bluetooth) {
+        console.log("Web Bluetooth API is not available in this browser!");
+        bleStateContainer.innerHTML = "Web Bluetooth API is not available in this browser!";
+        return false;
+    }
+    console.log('Web Bluetooth API supported in this browser.');
+    return true;
+}
+
+// Connect to BLE Device and Enable Notifications
+function connectToDevice() {
+    console.log('Initializing Bluetooth...');
+    navigator.bluetooth.requestDevice({
+        filters: [{ name: deviceName }],
+        optionalServices: [bleService]
+    })
+    .then(device => {
+        console.log('Device Selected:', device.name);
+        bleStateContainer.innerHTML = 'Connected to device ' + device.name;
+        bleStateContainer.style.color = "#24af37";
+        device.addEventListener('gattservicedisconnected', onDisconnected);
+        return device.gatt.connect();
+    })
+    .then(gattServer => {
+        bleServer = gattServer;
+        console.log("Connected to GATT Server");
+        return bleServer.getPrimaryService(bleService);
+    })
+    .then(service => {
+        bleServiceFound = service;
+        console.log("Service discovered:", service.uuid);
+        return service.getCharacteristic(ledCharacteristic);
+    })
+    .then(characteristic => {
+        console.log("LED characteristic discovered:", characteristic.uuid);
+        // No notifications for sensor characteristic, just read its value if needed
+    })
+    .catch(error => {
+        console.log('Error: ', error);
+    });
+}
+
+function onDisconnected(event) {
+    console.log('Device Disconnected:', event.target.device.name);
+    bleStateContainer.innerHTML = "Device disconnected";
+    bleStateContainer.style.color = "#d13a30";
+    connectToDevice();
+}
+
+// Write the PWM value to the ESP32 BLE Characteristic
+function writePwmValue(value) {
+    if (bleServer && bleServer.connected) {
+        bleServiceFound.getCharacteristic(ledCharacteristic)
+        .then(characteristic => {
+            console.log("Found the LED characteristic: ", characteristic.uuid);
+            const data = new Uint8Array([value]);
+            return characteristic.writeValue(data);
+        })
+        .then(() => {
+            latestValueSent.innerHTML = value;
+            console.log("Value written to LED characteristic:", value);
+        })
+        .catch(error => {
+            console.error("Error writing to the LED characteristic: ", error);
+        });
+    } else {
+        console.error("Bluetooth is not connected. Cannot write to characteristic.");
+        window.alert("Bluetooth is not connected. Cannot write to characteristic. Connect to BLE first!");
+    }
+}
+
+function disconnectDevice() {
+    console.log("Disconnect Device.");
+    if (bleServer && bleServer.connected) {
+        bleServer.disconnect()
+            .then(() => {
+                console.log("Device Disconnected");
+                bleStateContainer.innerHTML = "Device Disconnected";
+                bleStateContainer.style.color = "#d13a30";
+            })
+            .catch(error => {
+                console.log("An error occurred:", error);
+            });
+    } else {
+        console.error("Bluetooth is not connected.");
+        window.alert("Bluetooth is not connected.");
+    }
+}
